0cfe9507bbbfe1fb09d9ebe8708c8d3a3f59e6f8
pramitha1166 <63492955+pramitha1166@users.noreply.github.com>
2021-03-21 02:12:49 +0530

    Exception handling for 404 | HttpStatus  handling

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,13 +1,16 @@
 package com.example.demo.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -21,12 +24,23 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public User addNewUser(@RequestBody User user) {
-		return service.save(user);
+	public ResponseEntity<Object> addNewUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
+
 	}
 	
 	@GetMapping("/users/{id}")
 	public User getUserById(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+
+		if(user==null) {
+			throw new UserNotFoundException("id-"+id);
+		}
+
+		return user;
 	}
 }
